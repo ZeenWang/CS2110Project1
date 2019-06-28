e526b3482ea7eaf58e2f6ee22b97fe65ab84b465
kurtchua505 <kurtmb360@gmail.com>
2019-06-27 21:29:02 -0400

    created PersonTest class, no test cases (partner:zeen wang)

---
+++ src/a1/PersonTest.java
@@ -0,0 +1,49 @@
+package a1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PersonTest {
+
+	@Test
+	void personTest() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void nameTest() {
+		
+		
+	}
+
+	@Test
+	void motherTest() {
+		
+	}
+	
+	@Test
+	void fatherTest() {
+		
+	}
+	
+	@Test
+	void birthYearTest() {
+		
+	}
+	
+	@Test
+	void birthMonthTest() {
+		
+	}
+	
+	@Test
+	void birthDayTest() {
+		
+	}
+	
+	@Test
+	void numChildrenTest() {
+		
+	}
+}
