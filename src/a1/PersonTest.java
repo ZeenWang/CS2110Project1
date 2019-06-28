b136438a997de516e3cb8577e3412e148eef5ad9
Kurt Chua <kfb34@cornell.edu>
2019-06-27 21:44:54 -0400

    added new person ezra in personTest

--- src/a1/PersonTest.java
+++ src/a1/PersonTest.java
@@ -13,9 +13,17 @@ class PersonTest {
 		Person kurt = new Person("Kurt",1999,4,12);
 		assertEquals("Kurt", kurt.name());
 		assertEquals(1999, kurt.birthYear());
-		assertEquals(4,kurt.birthMonth());
-		assertEquals(12,kurt.birthDay());
+		assertEquals(4, kurt.birthMonth());
+		assertEquals(12, kurt.birthDay());
 		
+		// test for a new person Ezra
+		Person ezra = new Person("Ezra",1807,1,11);
+		assertEquals("Ezra", ezra.name());
+		assertEquals(1807, ezra.birthYear());
+		assertEquals(1, ezra.birthMonth());
+		assertEquals(11, ezra.birthDay());
+		
+	    
 	}
 	
 	@Test
