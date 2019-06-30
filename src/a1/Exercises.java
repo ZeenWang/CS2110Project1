f308bb7d7416f5b31a14f52076440c3356cb7646
zeenwang <zw587@cornell.edu>
2019-06-30 13:26:22 -0400

    Wrote the method signatures and their Javadocs.

---
+++ src/a1/Exercises.java
@@ -0,0 +1,65 @@
+package a1;
+
+public class Exercises {
+	
+	/**
+	 * Tell whether s is the same backwards and forwards.
+	 * @param s a String.
+	 * @return true if s is the same backwards and forwards.
+	 */
+	public static boolean isPalindrome(String s) {
+		throw new NotImplementedError();
+		
+	}
+	
+	/**
+	 * Remove all whitespace and punctuation in a String.
+	 * @param s a String.
+	 * @return a copy of s with all whitespace and punctuation removed.
+	 */
+	public static String normalize(String s) {
+		throw new NotImplementedError();
+		
+	}
+	
+	/**
+	 * Calculate the median of the values.
+	 * @param values an integer array, representing the sample. 
+	 * @return the median of the values.
+	 */
+	public static int median(int[ ] values) {
+		throw new NotImplementedError();
+		
+	}
+	
+	/**
+	 * Count the number of the 0 in a series of numbers.
+	 * @param values an integer array, representing the sample. 
+	 * @return the number of 0’s in values.
+	 */
+	public static int numZeros(int[ ] values) {
+		throw new NotImplementedError();
+		
+	}
+	
+	/**
+	 * Calculate the mean.
+	 * @param values an integer array, representing the sample. 
+	 * @return the mean of the values.
+	 */
+	public static double mean(int[ ] values) {
+		throw new NotImplementedError();
+		
+	}
+	
+	/** 
+	 * Tell whether the numbers on the diagonal of a square matrix share a same value.
+	 * Prerequisite: requires values to be a square matrix (that is, for all i, values[i].length== values.length).
+	 * @param values a 2-dimensional integer array, representing a matrix. 
+	 * @return true if all of the values on the diagonal are the same.
+	 */
+	public static boolean hasConstDiagonal(int[ ][ ] values) {
+		throw new NotImplementedError();
+		
+	}
+}
