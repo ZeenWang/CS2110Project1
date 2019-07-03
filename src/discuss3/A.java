7cb15e8931f12799936ec533728849a99270db7d
Kurt Chua <kfb34@cornell.edu>
2019-07-03 01:28:47 -0400

    added method calls from #2

--- src/discuss3/A.java
+++ src/discuss3/A.java
@@ -2,24 +2,29 @@ package discuss3;
 
 public class A {
 	public static double p(int x) {
-		int y=x;
+		int y =x;
 		try {
 			System.out.println("six");
 			y = 5/x;
 			System.out.println("five");
 			return 5/(x+2);
-		}catch(RuntimeException e) {
+		} catch (RuntimeException e) {
+			System.out.println("four");
+			y = 5/(x+1);
 			System.out.println("three");
 		}
+		
 		System.out.println("two");
-		y=4/x;
+		y = 4/x;
 		System.out.println("one");
 		return 1/x;
+	
 	}
-
+	
 	public static void main(String[] args) {
-		p(0);
-		p(-2);
+		p(-2); // passes
+		p(0); // throws new ArithmeticException
+		
 	}
 
 }
