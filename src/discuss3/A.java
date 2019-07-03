cd1f137d3ca481058be959551bc4bc31ecea7e51
Kurt Chua <kfb34@cornell.edu>
2019-07-03 00:55:05 -0400

    added implementation to A

--- src/discuss3/A.java
+++ src/discuss3/A.java
@@ -1,10 +1,25 @@
 package discuss3;
 
 public class A {
+	public static double p(int x) {
+		int y=x;
+		try {
+			System.out.println("six");
+			y = 5/x;
+			System.out.println("five");
+			return 5/(x+2);
+		}catch(RuntimeException e) {
+			System.out.println("three");
+		}
+		System.out.println("two");
+		y=4/x;
+		System.out.println("one");
+		return 1/x;
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		p(0);
+		p(-2);
 	}
 
 }
