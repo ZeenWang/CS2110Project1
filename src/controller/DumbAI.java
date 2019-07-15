8c4e9147a66c3391e5395662663678345a118f10
Kurt Chua <kfb34@cornell.edu>
2019-07-15 08:27:50 -0400

    implemented my version of DumbAI

--- src/controller/DumbAI.java
+++ src/controller/DumbAI.java
@@ -23,10 +23,13 @@ public class DumbAI extends Controller {
 		super.delay();
 		for(int i=0; i<Board.NUM_ROWS;i++) {
 			for(int j=0; j<Board.NUM_ROWS;j++) {
-				if(g.getBoard().get(i, j)==null)
-					return new Location(i, j);
+				Location location = new Location(i,j);
+				if(g.getBoard().get(i, j)==null) {
+					return location;
 			}
 		}
+	}
+		super.delay();
 		return null;
 	}
 }
